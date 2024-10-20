dc0f289e4fd807818656a181c717dedb75b10fd4
jguimeram <jguimera54@gmail.com>
2024-10-20 07:56:48 +0200

    boss test

--- dist/Entities/Boss.ts
+++ dist/Entities/Boss.ts
@@ -3,7 +3,6 @@ import { Vector2 } from "../Vectors/Vector2.js";
 
 
 export class Boss extends Entity{
-
    constructor(pos: Vector2, color:string, radius: number){
         super(pos,color,radius)
     }
